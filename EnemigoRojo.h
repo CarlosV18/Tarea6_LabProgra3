7f58084da6c02191b5e2f291b5ccb2c2d40411a1
Carlos Villanueva <prince.vill10@gmail.com>
2015-03-20 12:28:46 -0600

    first commit

---
+++ EnemigoRojo.h
@@ -0,0 +1,19 @@
+#ifndef ENEMIGOROJO_H
+#define ENEMIGOROJO_H
+#include <fstream>
+#include <stdlib.h>
+
+#include "Enemigo.h"
+
+class EnemigoRojo : public Enemigo
+{
+    public:
+        EnemigoRojo(SDL_Renderer* renderer,list<Personaje*> *personajes);
+        virtual ~EnemigoRojo();
+
+        void act();
+    protected:
+    private:
+};
+
+#endif // ENEMIGOROJO_H
---
+++ EnemigoVerde.h
@@ -0,0 +1,18 @@
+#ifndef ENEMIGOVERDE_H
+#define ENEMIGOVERDE_H
+#include <fstream>
+#include <stdlib.h>
+
+#include "Enemigo.h"
+
+class EnemigoVerde : public Enemigo
+{
+    public:
+        EnemigoVerde(SDL_Renderer* renderer,list<Personaje*> *personajes);
+        virtual ~EnemigoVerde();
+        void act();
+    protected:
+    private:
+};
+
+#endif // ENEMIGOVERDE_H
